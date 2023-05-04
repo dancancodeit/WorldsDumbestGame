607023774d52e2aec9a6aa4aeb2eb03f5d552199
Daniel Miller <dancancodeit@gmail.com>
2023-05-03 23:33:37 -0400

    add more

--- src/Scoreboard.tsx
+++ src/Scoreboard.tsx
@@ -1,18 +1,17 @@
 import { useEffect, useState } from "react";
 
-// type Score = {
-//   playerName: string,
-//   score: number
-// }
+type Score = {
+  playerName: string,
+  score: number
+}
 function Scoreboard () {
-  const [scores, setScores] = useState<any>([])
+  const [scores, setScores] = useState<Score[]>([])
 
   useEffect(() => {
     const getScores = async () => {
       const data = await fetch('/.netlify/functions/getScores');
-      const respData = await data.json();
-      console.log(respData);
-      setScores(respData)
+      const respData = await data.json()
+      setScores(respData as Score[])
     }
 
     getScores();
