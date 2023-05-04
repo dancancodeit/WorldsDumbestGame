76a4904eacc5aee37fccebd41780db04d04b11fe
Daniel Miller <danielmiller@Daniels-MacBook-Pro.local>
2023-05-03 23:17:02 -0400

    just let me build

--- src/Scoreboard.tsx
+++ src/Scoreboard.tsx
@@ -1,23 +1,24 @@
 import { useEffect, useState } from "react";
 
-type Score = {
-  playerName: string,
-  score: number
-}
+// type Score = {
+//   playerName: string,
+//   score: number
+// }
 function Scoreboard () {
-  const [scores, setScores] = useState<Score[]>([])
+  const [scores, setScores] = useState<any>([])
 
   useEffect(() => {
     const getScores = async () => {
       const data = await fetch('/.netlify/functions/getScores');
       console.log(data);
+      setScores(data)
     }
 
     getScores();
 
   }, []);
 
-  return <>hello</>;
+  return <>{scores}</>;
 }
 
 
